16de7eb5edf42e2eb079762cfcfa807e08590f1d
aiskhak <62344361+aiskhak@users.noreply.github.com>
2021-04-28 14:30:56 -0400

    Update fortfp.f90

--- fortfp.f90
+++ fortfp.f90
@@ -1,190 +1,185 @@
 !------------------------------------------------------------------------
-! FortFP ----------------------------------------------------------------
+! fortfp ----------------------------------------------------------------
 !    Fortran module that reads weights and biases -----------------------
 !    and performs a forward propagation ---------------------------------
 !------------------------------------------------------------------------
 
-MODULE FortFP
+module fortfp
    
-   IMPLICIT NONE
+   implicit none
    
-   INTEGER, PARAMETER :: dp=selected_real_KIND(13)
-
    ! activation function type
-   TYPE :: af
-      PROCEDURE(ndarray), POINTER, NOPASS :: f
-   END TYPE af
+   type :: af
+      procedure(ndarray), pointer, nopass :: f
+   end type af
 
    ! array of function pointers for activation functions
-   TYPE (af), DIMENSION(:), ALLOCATABLE :: activfuncs
+   type (af), dimension(:), allocatable :: activfuncs
    
    ! layer type
-   TYPE :: layer
+   type :: layer
       
-      REAL(kind=dp), DIMENSION (:,:), POINTER :: w, wd               ! matrix of weights
-      REAL(kind=dp), DIMENSION (:), POINTER :: b, bd                 ! array of biases
-      REAL(kind=dp), DIMENSION (:), POINTER :: y                     ! array of outputs
-      PROCEDURE(ndarray), PUBLIC, NOPASS, POINTER :: af => NULL()    ! activation function
+      real(8), dimension (:,:), pointer :: w, wd               		! matrix of weights
+      real(8), dimension (:), pointer :: b, bd                 		! array of biases
+      real(8), dimension (:), pointer :: y                     		! array of outputs
+      procedure(ndarray), public, nopass, pointer :: af => null()	! activation function
    
-   END TYPE layer
-
-   ! layer is declared as target (can be pointed efficiently)
-   TARGET :: layer
+   end type layer
 
    ! making pointers for working with input and output vectors
-   REAL(kind=dp), DIMENSION (:), POINTER :: xp => NULL(), yp => NULL()
+   real(8), dimension (:), pointer :: xp => null(), yp => null()
    
    ! neural network type: array of layers
-   TYPE :: nn
+   type :: nn
       
-      TYPE(layer), DIMENSION (:), ALLOCATABLE :: layers
+      type(layer), dimension (:), allocatable :: layers
    
-   CONTAINS 
+   contains 
       
       ! initialize neural network
-      PROCEDURE, PUBLIC :: init
+      procedure, public :: init
       
       ! perform forward propagation
-      PROCEDURE, PUBLIC :: query
+      procedure, public :: query
 
-   END TYPE nn
+   end type nn
    
    ! dynamic arrays type
-   TYPE :: dyn_arr
+   type :: dyn_arr
 
-      REAL(kind=dp), DIMENSION(:), ALLOCATABLE :: v
-      REAL(kind=dp), DIMENSION(:,:), ALLOCATABLE :: m
+      real(8), dimension(:), allocatable :: v
+      real(8), dimension(:,:), allocatable :: m
 
-   END TYPE dyn_arr
+   end type dyn_arr
    
    ! interface for activation functions
-   ABSTRACT INTERFACE
+   abstract interface
 
-      FUNCTION ndarray(n)
+      function ndarray(n)
          
-         IMPORT dp
-         IMPORT af
-         REAL(kind=dp), DIMENSION (:), INTENT(in) :: n
-         REAL(kind=dp), DIMENSION (SIZE(n)) :: ndarray
+         import af
+         real(8), dimension (:), intent(in) :: n
+         real(8), dimension (size(n)) :: ndarray
 
-      END FUNCTION ndarray
+      end function ndarray
 
-   END INTERFACE
+   end interface
 
-   PRIVATE :: readwb, init, query
+   private :: readwb, init, query
 
-CONTAINS
+contains
 
    !=====================================================================
    ! initialization of neural network
-   SUBROUTINE init(this, layers, activ_func)
+   subroutine init(this, layers, activ_func)
       
-      CLASS (nn), INTENT(inout) :: this
+      class (nn), intent(inout) :: this
 
       ! array containing structure of layers, e.g., [100, 20, 30, 10]
       ! 100 input neurons; 20 neurons in first hidden layer;
       ! 30 neurons in second hidden layer; 10 neurons in output layer
-      INTEGER, DIMENSION (:), INTENT(in) :: layers
-      INTEGER :: i, nin, nout, nl
+      integer, dimension (:), intent(in) :: layers
+      integer :: i, nin, nout, nl
 
       ! list of activation functions passed in
-      TYPE (af), DIMENSION(0:), INTENT(in) :: activ_func
-      TYPE (dyn_arr) :: wb
+      type (af), dimension(0:), intent(in) :: activ_func
+      real(8) :: sd
+      type (dyn_arr) :: wb
 
       ! check if number of activation functions match number of layers
-      IF (SIZE(activ_func) /= SIZE(layers)) THEN
-         PRINT *, "The number of layers and activation functions do not match"
-         STOP
-      ENDIF
+      if (size(activ_func) /= size(layers)) then
+         print *, "The number of layers and activation functions do not match"
+         stop
+      endif
       
       ! number of layers and neurons
-      nl = SIZE(layers)
+      nl = size(layers)
       nin = layers(1)
       nout = layers(nl)
       
       ! layers' numbers begin from 0
-      ALLOCATE (this%layers(0:nl-1))
+      allocate (this%layers(0:nl-1))
 
       ! loop over layers
-      DO i=1,nl-1
+      do i=1,nl-1
          
          ! allocate weights
-         ALLOCATE (this%layers(i)%w(layers(i+1),layers(i)))
-         ALLOCATE (this%layers(i)%wd(layers(i+1),layers(i)))
+         allocate (this%layers(i)%w(layers(i+1),layers(i)))
+         allocate (this%layers(i)%wd(layers(i+1),layers(i)))
 
          ! allocate biases
-         ALLOCATE (this%layers(i)%b(layers(i+1)))
-         ALLOCATE (this%layers(i)%bd(layers(i+1)))
+         allocate (this%layers(i)%b(layers(i+1)))
+         allocate (this%layers(i)%bd(layers(i+1)))
 
          ! set to 0
-         this%layers(i)%wd = 0._dp
-         this%layers(i)%bd = 0._dp
+         this%layers(i)%wd = 0.d0
+         this%layers(i)%bd = 0.d0
 
          ! allocate outputs
-         ALLOCATE (this%layers(i)%y(layers(i+1)))
+         allocate (this%layers(i)%y(layers(i+1)))
 
-         ! read weights and biases from files
-         CALL readwb(i, SHAPE(this%layers(i)%w), wb)
-         CALL readwb(i, SHAPE(this%layers(i)%b), wb)
+         ! read weights and biases
+         call readwb(i, shape(this%layers(i)%w), wb)
+         call readwb(i, shape(this%layers(i)%b), wb)
          this%layers(i)%w = wb%m
          this%layers(i)%b = wb%v
 
-         DEALLOCATE (wb%v,wb%m)
+         deallocate (wb%v,wb%m)
 
          ! assign activation functions
          this%layers(i)%af => activ_func(i)%f
 
-      ENDDO
+      enddo
 
       ! manually create layer 0
-      ALLOCATE (this%layers(0)%y(nin))
+      allocate (this%layers(0)%y(nin))
 
       ! no weights in layer 0
-      ALLOCATE (this%layers(0)%w(0,0), this%layers(0)%wd(0,0)) 
-      ALLOCATE (this%layers(0)%b(0), this%layers(0)%bd(0))
+      allocate (this%layers(0)%w(0,0), this%layers(0)%wd(0,0)) 
+      allocate (this%layers(0)%b(0), this%layers(0)%bd(0))
 
-      ! activation function for layer 0 (very likely NULL)
+      ! activation function for layer 0 (very likely null)
       this%layers(0)%af => activ_func(0)%f
 
-   END SUBROUTINE init
+   end subroutine init
    
    !=====================================================================
    ! given input x, performs forward propagation with results in this%y
-   SUBROUTINE query(this, x)
+   subroutine query(this, x)
 
-      CLASS(nn), INTENT(inout) :: this
-      REAL(kind=dp), DIMENSION(:), INTENT(in) :: x
-      INTEGER :: i, n, lm, ln
-      INTEGER :: j, k
-      REAL(kind=dp) :: sum_j
+      class(nn), intent(inout) :: this
+      real(8), dimension(:), intent(in) :: x
+      integer :: i, n, lm, ln
+      integer :: j, k
+      real(8) :: sum_j
       
       ! number of weight matrices
       ! layer 0 does not have it, so the number is 1 unit less
-      n = SIZE(this%layers) - 1
+      n = size(this%layers) - 1
       
       ! copy x into y(0)
       this%layers(0)%y = x
 
-      ! if activation function for inputs is used (very likely NULL)
-      IF (ASSOCIATED(this%layers(0)%af)) &
+      ! if activation function for inputs is used (very likely null)
+      if (associated(this%layers(0)%af)) &
            this%layers(0)%y = this%layers(0)%af (this%layers(0)%y)
 
       ! loop over layers
-      DO i=1,n
+      do i=1,n
 
          xp => this%layers(i-1)%y
          yp => this%layers(i)%y
 
-         lm = UBOUND(this%layers(i)%w, 1)
-         ln = UBOUND(this%layers(i)%w, 2)
+         lm = ubound(this%layers(i)%w, 1)
+         ln = ubound(this%layers(i)%w, 2)
          yp = this%layers(i)%b
 
          ! matrix-vector multiplication: lapack library
-         !CALL dgemv("N", lm, ln, 1._dp, this%layers(i)%w, lm, xp, 1, 1._dp, yp, 1)  
+         !call dgemv("N", lm, ln, 1.d0, this%layers(i)%w, lm, xp, 1, 1.d0, yp, 1)  
 
          ! uncomment this staff if there is no access to lapack
          do k=1,lm
-            sum_j = 0._dp
+            sum_j = 0.d0
             do j=1,ln
                sum_j = sum_j + this%layers(i)%w(k,j)*xp(j)
             enddo
@@ -194,61 +189,59 @@ CONTAINS
          ! apply activation
          yp = this%layers(i)%af(yp)
          
-      ENDDO
+      enddo
 
-
-   END SUBROUTINE query
+   end subroutine query
 
    ! =====================================================================
    ! read weights and biases from file
-   SUBROUTINE readwb(n_skip, frm, arr)
+   subroutine readwb(n_skip, frm, arr)
 
-      INTEGER, INTENT(in) :: n_skip
-      TYPE (dyn_arr), INTENT(inout) :: arr
-      REAL(kind=dp), DIMENSION(:), ALLOCATABLE :: nd
-      INTEGER, DIMENSION(:), INTENT(in) :: frm
-      INTEGER, PARAMETER :: ki = selected_int_KIND(18)
-      INTEGER (kind=ki) :: samples, n, nm, osize
-      INTEGER :: i, lm, ln, j, k, nsum
+      integer, intent(in) :: n_skip
+      type (dyn_arr), intent(inout) :: arr
+      real(8), dimension(:), allocatable :: nd
+      integer, dimension(:), intent(in) :: frm
+      integer(4) :: samples, n, nm, osize
+      integer(4) :: i, lm, ln, j, k, nsum
 
       ! nm = 2 for weights; nm = 1 for biases
-      nm = SIZE(frm)
+      nm = size(frm)
       
       ! allocate arrays for weights and open file
-      IF (nm == 2) THEN
-         samples = INT(frm(1), ki)*INT(frm(2), ki)
+      if (nm == 2) then
+         samples = int(frm(1))*int(frm(2))
          osize = samples	! recording original size
          ! in case both dimensions are odd
-         IF (MOD(samples, 2).EQ.1) samples = samples + 1
-         ALLOCATE (arr%m(frm(1), frm(2)))
-         OPEN (1111, FILE = 'wb/w_0', status = 'old')
+         if (mod(samples, 2) == 1) samples = samples + 1
+         allocate (arr%m(frm(1), frm(2)))
+         open (1111, file = 'wb/w_0', status = 'old')
       ! allocate arrays for biases
-      ELSE
-         samples=INT(frm(1),ki)
+      else
+         samples=int(frm(1))
          osize = samples   	! recording original size
          ! in case dimension is odd
-         IF (MOD(samples,2).EQ.1) samples = samples + 1
-         ALLOCATE (arr%v(frm(1)))
-         OPEN (1111, FILE = 'wb/b_0', status = 'old')
-      ENDIF
+         if (mod(samples,2) == 1) samples = samples + 1
+         allocate (arr%v(frm(1)))
+         open (1111, file = 'wb/b_0', status = 'old')
+      endif
       
       ! array for values
-      ALLOCATE (nd(samples))
+      allocate (nd(samples))
       nd = 0
 
       ! skip necessary lines
-      IF (n_skip > 1) THEN
-         REWIND (1111)
-         DO i=1,n_skip-1
-            READ (1111,*)
-         END DO
-      END IF
+      if (n_skip > 1) then
+         rewind (1111)
+         do i=1,n_skip-1
+            read (1111,*)
+         end do
+      end if
       ! read values from file
-      READ (1111,*) nd
-      CLOSE (1111)
+      read (1111,*) nd
+      close (1111)
 
-      ! weights	
-      IF (nm == 2) THEN
+      ! weights
+      if (nm == 2) then
          ln = size(arr%m, dim=2)
          lm = size(arr%m, dim=1)
          nsum = 1
@@ -259,11 +252,11 @@ CONTAINS
             enddo
          enddo
       ! biases
-      ELSE
+      else
          arr%v = nd(1:osize)
-      ENDIF
+      endif
 
-   END SUBROUTINE readwb
+   end subroutine readwb
    !=====================================================================
 
    !---------------------------------------------------------------------
@@ -273,78 +266,78 @@ CONTAINS
    !---------------------------------------------------------------------
    
    !=====================================================================  
-   PURE FUNCTION sigmoid(x)
+   pure function sigmoid(x)
       
       ! sigmoid
-      REAL(kind=dp), DIMENSION (:), INTENT(in) :: x
-      REAL(kind=dp), DIMENSION (SIZE(x)) :: sigmoid
+      real(8), dimension (:), intent(in) :: x
+      real(8), dimension (size(x)) :: sigmoid
       
-      sigmoid = EXP(-x)
-      sigmoid = 1._dp + sigmoid
-      sigmoid = 1._dp / sigmoid
+      sigmoid = exp(-x)
+      sigmoid = 1.d0 + sigmoid
+      sigmoid = 1.d0 / sigmoid
    
-   END FUNCTION sigmoid
+   end function sigmoid
 
    ! =====================================================================
-   PURE FUNCTION htan (x)
+   pure function htan (x)
       
       ! hyperbolic tan
-      REAL(kind=dp), DIMENSION (:), INTENT(in) :: x
-      REAL(kind=dp), DIMENSION (SIZE(x)) :: htan
-      htan = TANH(x)
+      real(8), dimension (:), intent(in) :: x
+      real(8), dimension (size(x)) :: htan
+      htan = tanh(x)
 
-   END FUNCTION htan
+   end function htan
 
    ! =====================================================================
-   PURE FUNCTION smax (y)
+   pure function smax (y)
       
       ! softmax
-      REAL(kind=dp), DIMENSION(:), INTENT(in) :: y
-      REAL(kind=dp), DIMENSION (SIZE(y)) :: smax
-      REAL(kind=dp) :: sum_smax, l1, l2
-      l1 = 1.e-9_dp
-      l2 = 0.999999999_dp
-      smax = EXP(y)
-      sum_smax = SUM(smax)
+      real(8), dimension(:), intent(in) :: y
+      real(8), dimension (size(y)) :: smax
+      real(8) :: sum_smax, l1, l2
+      l1 = 1.d-9
+      l2 = 0.999999999d0
+      smax = exp(y)
+      sum_smax = sum(smax)
       smax = smax / sum_smax
-      WHERE (smax > l2) smax = l2
-      WHERE (smax < l1) smax = l1
+      where (smax > l2) smax = l2
+      where (smax < l1) smax = l1
 
-   END FUNCTION smax
+   end function smax
 
    ! =====================================================================
-   PURE FUNCTION lrelu(x)
+   pure function lrelu(x)
 
       ! leaky relu
-      REAL(kind=dp), DIMENSION (:), INTENT(in) :: x
-      REAL(kind=dp), DIMENSION (SIZE(x)) :: lrelu
+      real(8), dimension (:), intent(in) :: x
+      real(8), dimension (size(x)) :: lrelu
       lrelu = x
-      WHERE (x < 0._dp) lrelu = 0.01_dp*x
+      where (x < 0.d0) lrelu = 0.01d0*x
 
-   END FUNCTION lrelu
+   end function lrelu
 
    ! =====================================================================
-   PURE FUNCTION relu(x)
+   pure function relu(x)
       
       ! relu
-      REAL(kind=dp), DIMENSION (:), INTENT(in) :: x
-      REAL(kind=dp), DIMENSION (SIZE(x)) :: relu
+      real(8), dimension (:), intent(in) :: x
+      real(8), dimension (size(x)) :: relu
 
       relu = x
-      WHERE ( x < 0._dp ) relu = 0._dp
+      where ( x < 0.d0 ) relu = 0.d0
 
-   END FUNCTION relu
+   end function relu
    ! =====================================================================
 
-   PURE FUNCTION no_activation(x)
+   pure function no_activation(x)
       
       ! no activation
-      REAL(kind=dp), DIMENSION (:), INTENT(in) :: x
-      REAL(kind=dp), DIMENSION (SIZE(x)) :: no_activation
+      real(8), dimension (:), intent(in) :: x
+      real(8), dimension (size(x)) :: no_activation
 
       no_activation = x
 
-   END FUNCTION no_activation
+   end function no_activation
    ! =====================================================================
 
-END MODULE FortFP
+end module fortfp
